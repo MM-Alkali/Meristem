479e3ed4315b3fc710c8a30b3f66be654a218b5b
omejeemmanuel1 <omejeemmanuel046@yahoo.com>
2023-07-18 11:33:27 +0100

    Frontend setup done

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,18 @@
+
+import NavBar from '../components/NavBar/NavBar'
+import Header from '../components/Header/Header'
+import HeroSection from '../components/HeroSection/HeroSection'
+import Footer from '../components/Footer/Footer'
+
+const LandingPage = () => {
+  return (
+    <>
+        <NavBar />
+        <Header />
+        <HeroSection />
+        <Footer />
+    </>
+  )
+}
+
+export default LandingPage
