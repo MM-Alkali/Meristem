b1a694a24f8c1a3ba0686d683bab6bfbad58088e
Muhammad Alkali Muawiya <123987902+MM-Alkali@users.noreply.github.com>
2023-07-23 14:19:47 +0100

    About Us Page

--- src/components/AboutUs/CoreValues.tsx
+++ src/components/AboutUs/CoreValues.tsx
@@ -1,55 +1,117 @@
 import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 import "./CoreValues.css";
 
 const CoreValues = () => {
-  const [activeDropdown, setActiveDropdown] = useState(null);
+  const [activeDropdown, setActiveDropdown] = useState(-1);
   const [modalVisible, setModalVisible] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(-1);
 
-  const toggleDropdown = (index: any) => {
-    setActiveDropdown(index === activeDropdown ? null : index);
+  const toggleDropdown = (index: number) => {
+    if (currentIndex === index) {
+      setCurrentIndex(-1);
+    } else {
+      setCurrentIndex(index);
+    }
   };
 
-  const toggleModal = () => {
+  const toggleModal = (String: string) => {
     setModalVisible(!modalVisible);
+    setCurrentIndex(-1);
+    if (String === "Our Board") {
+      setActiveDropdown(0);
+    } else if (String === "Our Leadership") {
+      setActiveDropdown(1);
+    } else if (String === "Our Vision") {
+      setActiveDropdown(2);
+    } else if (String === "Our Mission") {
+      setActiveDropdown(3);
+    } else if (String === "Our Philosophy") {
+      setActiveDropdown(4);
+    } else if (String === "Our Brand") {
+      setActiveDropdown(5);
+    } else if (String === "Brand Promise") {
+      setActiveDropdown(6);
+    } else if (String === "Subsidiary1") {
+      setActiveDropdown(7);
+    } else if (String === "Subsidiary2") {
+      setActiveDropdown(8);
+    } else if (String === "Subsidiary3") {
+      setActiveDropdown(9);
+    } else if (String === "Subsidiary4") {
+      setActiveDropdown(10);
+    } else if (String === "Subsidiary5") {
+      setActiveDropdown(11);
+    } else if (String === "Subsidiary6") {
+      setActiveDropdown(12);
+    } else {
+      setActiveDropdown(-1);
+    }
   };
 
   return (
     <div className="about-buttons">
-      <button onClick={() => toggleDropdown(0)}>Our Leadership</button>
-      <button onClick={() => toggleDropdown(1)}>Core Values</button>
-      <button onClick={() => toggleDropdown(2)}>Subsidiaries</button>
-
-      {/* Dropdown Content */}
-      <div
-        className={`dropdown-content ${
-          activeDropdown !== null ? "show-dropdown" : ""
-        }`}
-      >
-        {activeDropdown === 0 && (
+      <div className="about-button">
+        <button id="button-0" onClick={() => toggleDropdown(0)}>
+          Our Leadership
+        </button>
+        {currentIndex === 0 && (
           <div className="dropdown-content-buttons">
-            <button onClick={toggleModal}>Our Board</button>
-            <button onClick={toggleModal}>Our Leadership</button>
+            <button onClick={() => toggleModal("Our Board")}>Our Board</button>
+            <button onClick={() => toggleModal("Our Leadership")}>
+              Our Leadership
+            </button>
           </div>
         )}
+      </div>
 
-        {activeDropdown === 1 && (
+      <div className="about-button">
+        <button id="button-1" onClick={() => toggleDropdown(1)}>
+          Core Values
+        </button>
+        {currentIndex === 1 && (
           <div className="dropdown-content-buttons">
-            <button onClick={toggleModal}>Our Mission</button>
-            <button onClick={toggleModal}>Our Vision</button>
-            <button onClick={toggleModal}>Our Philosophy</button>
-            <button onClick={toggleModal}>Our Brand</button>
-            <button onClick={toggleModal}>Brand Promise</button>
+            <button onClick={() => toggleModal("Our Vision")}>
+              Our Vision
+            </button>
+            <button onClick={() => toggleModal("Our Mission")}>
+              Our Mission
+            </button>
+            <button onClick={() => toggleModal("Our Philosophy")}>
+              Our Philosophy
+            </button>
+            <button onClick={() => toggleModal("Our Brand")}>Our Brand</button>
+            <button onClick={() => toggleModal("Brand Promise")}>
+              Brand Promise
+            </button>
           </div>
         )}
+      </div>
 
-        {activeDropdown === 2 && (
+      <div className="about-button">
+        <button id="button-2" onClick={() => toggleDropdown(2)}>
+          Subsidiaries
+        </button>
+        {currentIndex === 2 && (
           <div className="dropdown-content-buttons">
-            <button onClick={toggleModal}>MERISTEM WEALTH MANAGEMENT</button>
-            <button onClick={toggleModal}>MERISTEM SECURITIES TRADING</button>
-            <button onClick={toggleModal}>MERISTEM INVESTMENT BANKING</button>
-            <button onClick={toggleModal}>MERISTEM REGISTRAR SERVICES</button>
-            <button onClick={toggleModal}>MERISTEM TRUSTEE SERVICES</button>
-            <button onClick={toggleModal}>MERISTEM LOANS AND LEASING</button>
+            <button onClick={() => toggleModal("Subsidiary1")}>
+              Meristem Wealth Management
+            </button>
+            <button onClick={() => toggleModal("Subsidiary2")}>
+              Meristem Securities Trading
+            </button>
+            <button onClick={() => toggleModal("Subsidiary3")}>
+              Meristem Investment Banking
+            </button>
+            <button onClick={() => toggleModal("Subsidiary4")}>
+              Meristem Registrar Services
+            </button>
+            <button onClick={() => toggleModal("Subsidiary5")}>
+              Meristem Trustee Services
+            </button>
+            <button onClick={() => toggleModal("Subsidiary6")}>
+              Meristem Loans and Leasing
+            </button>
           </div>
         )}
       </div>
@@ -58,14 +120,25 @@ const CoreValues = () => {
       {modalVisible && (
         <div className="modal-container">
           <div className="modal">
-            {/* Close sign */}
-            <button className="close-button" onClick={toggleModal}>
-              X
-            </button>
-
+            <div className="close-button">
+              <button onClick={() => toggleModal("Close")}>
+                <FaTimes />
+              </button>
+            </div>
             <div className="content">
-              {/* Render different modal content based on the active dropdown */}
               {activeDropdown === 0 && (
+                <div className="board-members">
+                  <h2>Our Board</h2>
+                  <p>Board goes here</p>
+                </div>
+              )}
+              {activeDropdown === 1 && (
+                <div className="leadership">
+                  <h2>Our Leadership</h2>
+                  <p>Leadership goes here</p>
+                </div>
+              )}
+              {activeDropdown === 2 && (
                 <div className="vision">
                   <h2>Vision</h2>
                   <p>
@@ -73,7 +146,7 @@ const CoreValues = () => {
                   </p>
                 </div>
               )}
-              {activeDropdown === 1 && (
+              {activeDropdown === 3 && (
                 <div className="mission">
                   <h2>Mission</h2>
                   <p>
@@ -84,13 +157,115 @@ const CoreValues = () => {
                   </p>
                 </div>
               )}
-              {activeDropdown === 2 && (
-                <div className="subsidiaries">
-                  <h2>Subsidiaries</h2>
+              {activeDropdown === 4 && (
+                <div className="philosophy">
+                  <h2>Philosophy</h2>
                   <p>
-                    MERISTEM WEALTH MANAGEMENT, MERISTEM SECURITIES TRADING,
-                    MERISTEM INVESTMENT BANKING, MERISTEM REGISTRAR SERVICES,
-                    MERISTEM TRUSTEE SERVICES, MERISTEM LOANS AND LEASING
+                    Guided by our belief that our success depends entirely on
+                    the extent to which our clients’ objectives are met, we put
+                    ourselves in the place of our clients and we serve them as
+                    we will serve ourselves when they are not happy we have
+                    failed and when they smile we also smile.
+                  </p>
+                  <p>
+                    We are a friendly company with up to date market information
+                    to grow wealth in good time in an atmosphere of
+                    transparency.
+                  </p>
+                </div>
+              )}
+              {activeDropdown === 5 && (
+                <div className="brand">
+                  <h2>Brand</h2>
+                  <p>
+                    The letters in MERISTEM reinforce dominating qualities of
+                    assurance, trust and maturity.
+                  </p>
+                  <p>
+                    The green background typifies growth, harmony as well as
+                    connotation to money.
+                  </p>
+                  <p>
+                    The three leaf elements supports the tacit meaning of the
+                    name which is pacesetting, productivity, multiplication,
+                    resilience, determination, futuristic and growth.
+                  </p>
+                </div>
+              )}
+              {activeDropdown === 6 && (
+                <div className="brand-promise">
+                  <h2>Brand Promise</h2>
+                  <p>
+                    At Meristem, we anchor “Clientocracy” at the heart of our
+                    core values, it is the foundation of our existence as an
+                    organization, a deep commitment to serve our clients
+                    extensively, understand and meet their needs, exceed their
+                    expectations and ultimately make them happy.
+                  </p>
+                  <p>
+                    The Brand Ideology, <span className="bold">CEPOEM </span>
+                    presents <span className="bold">Clientocracy</span>,
+                    <span className="bold"> Esprit de corp</span>,
+                    <span className="bold"> Professionalism and Integrity</span>
+                    ,<span className="bold"> Ownership</span>,
+                    <span className="bold"> Einstein</span>, and
+                    <span className="bold"> Meristar-delight</span>.
+                  </p>
+                </div>
+              )}
+              {activeDropdown === 7 && (
+                <div className="subsidiary">
+                  <h2>MERISTEM WEALTH MANAGEMENT</h2>
+                  <p>
+                    Information about MERISTEM WEALTH MANAGEMENT goes here
+                  </p>
+                </div>
+              )}
+              {activeDropdown === 8 && (
+                <div className="subsidiary">
+                  <h2>MERISTEM SECURITIES TRADING</h2>
+                  <p>
+                    Information about MERISTEM SECURITIES TRADING goes here
+                  </p>
+                </div>
+              )}
+              {activeDropdown === 9 && (
+                <div className="subsidiary">
+                  <h2>MERISTEM INVESTMENT BANKING</h2>
+                  <p>
+                    Information about MERISTEM INVESTMENT BANKING goes here
+                  </p>
+                </div>
+              )}
+              {activeDropdown === 10 && (
+                <div className="subsidiary">
+                  <h2>MERISTEM REGISTRAR SERVICES</h2>
+                  <p>
+                    Information about MERISTEM REGISTRAR SERVICES goes here
+                  </p>
+                </div>
+              )}
+              {activeDropdown === 11 && (
+                <div className="subsidiary">
+                  <h2>MERISTEM TRUSTEE SERVICES</h2>
+                  <p>
+                    Information about MERISTEM TRUSTEE SERVICES goes here
+                  </p>
+                </div>
+              )}
+              {activeDropdown === 12 && (
+                <div className="subsidiary">
+                  <h2>MERISTEM LOANS AND LEASING</h2>
+                  <p>
+                    Information about MERISTEM LOANS AND LEASING goes here
+                  </p>
+                </div>
+              )}
+              {activeDropdown === 13 && (
+                <div className="subsidiary">
+                  <h2>Another Subsidiary</h2>
+                  <p>
+                    Information about another subsidiary goes here
                   </p>
                 </div>
               )}
